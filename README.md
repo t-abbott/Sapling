606c67f124fab382fc92b3faef45e86d52c765ba
tabbeyy <tommcmin@gmail.com>
2022-09-06 12:55:43 +0100

    Add if/then statements and other operators

--- README.md
+++ README.md
@@ -5,8 +5,16 @@ Inspired by [mini-ml](https://dl.acm.org/doi/10.1145/319838.319847)
 and Neel Krishnaswami's [blog post](https://semantic-domain.blogspot.com/2015/07/how-to-implement-spreadsheet.html).
 
 ## Dependencies
+
 - `FsLexYacc`
 
 ## Use
+
 `dotnet run -- <file>`
 
+## TODO
+
+- fix parser ambiguity errors
+- make application have higher precedence than addition etc
+- make closure ToString nice
+- add a test rig on the example files
--- src/Syntax.fs
+++ src/Syntax.fs
@@ -3,13 +3,49 @@ namespace Incremental
 module Expr = 
     type Name = string
 
+    type Op = 
+        | LessThan
+        | GreaterThan
+        | Equals
+        | And
+        | Or
+        | Add
+        | Sub
+        | Mult
+        | Div
+        | Mod
+        with
+            override this.ToString () = 
+                match this with
+                | LessThan -> "<"
+                | GreaterThan -> ">"
+                | Equals -> "="
+                | And -> "and"
+                | Or -> "or"
+                | Add -> "+"
+                | Sub -> "-"
+                | Mult -> "*"
+                | Div -> "/"
+                | Mod -> "%"
+
+            member this.hasNumericArgs =
+                match this with
+                | And | Or -> false
+                | _ -> true
+
+            member this.asBooleanArgs = 
+                match this with
+                | And | Or -> true
+                | _ -> false
+
+
     type T =
         | Unit 
         | Bool of bool
         | Number of float
         | Var of Name
         | LetIn of Name * T * T
-        | Binop of T * Name * T
+        | Binop of T * Op * T
         | IfThen of T * T * T
         | Fun of Name * T
         | Apply of T * T
@@ -24,7 +60,7 @@ module Expr =
                 | LetIn (name, e, body) ->
                     sprintf "let %s = %s in %s" name (e.ToString ()) (body.ToString ())
                 | Binop (l, op, r) ->
-                    sprintf "(%s %s %s)" (l.ToString ()) op (r.ToString ())
+                    sprintf "(%s %s %s)" (l.ToString ()) (op.ToString ()) (r.ToString ())
                 | IfThen (cond, e1, e2) ->
                     sprintf "(if %s then %s else %s)" (cond.ToString ()) (e1.ToString ()) (e2.ToString ())
                 | Fun (name, body) -> 
