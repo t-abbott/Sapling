7bcd181d90741ae286ab66e3a3ec2a80c92bc98b
tabbeyy <tommcmin@gmail.com>
2022-09-12 11:56:50 +0100

    Add cell type

--- src/Syntax.fs
+++ src/Syntax.fs
@@ -1,8 +1,10 @@
 namespace Incremental
 
 module Expr =
+    ///
     type Name = string
 
+    ///
     type Op =
         | LessThan
         | GreaterThan
@@ -40,7 +42,7 @@ module Expr =
             | Or -> true
             | _ -> false
 
-
+    ///
     type T =
         | Unit
         | Bool of bool
@@ -71,3 +73,51 @@ module Expr =
             | Bool _
             | Number _ -> true
             | _ -> false
+
+    ///
+    and Cell =
+        {
+            id: System.Guid
+
+            /// The expression
+            mutable body: T
+
+            /// The cached value of this `Cell`
+            mutable value: T option
+
+            /// A list of `Cells` that `this` depends on
+            mutable reads: Cell list
+
+            /// A list of `Cells` that depend on `this`
+            mutable observers: Cell list
+        }
+
+        static member fromExpr e =
+            { id = System.Guid.NewGuid()
+              body = e
+              value = None
+              reads = []
+              observers = [] }
+
+        /// Remove `cell` as an observer of `observedCell`
+        static member removeObserver cell observedCell =
+            observedCell.observers <- List.filter (fun o -> o.id <> cell.id) observedCell.observers
+
+        /// Invalidate `this` cell and it's dependencies
+        member this.Invalidate() =
+            List.iter (fun (o: Cell) -> o.Invalidate()) this.observers
+            List.iter (fun (o: Cell) -> Cell.removeObserver this o) this.reads
+            this.value <- None
+            this.observers <- []
+            this.reads <- []
+
+        /// Get the current value of `this`
+        member this.Get =
+            match this.value with
+            | Some e -> e
+            | None -> failwith "not implemented"
+
+        /// Change the body of `this`
+        member this.Set expr =
+            this.Invalidate()
+            this.body <- expr
